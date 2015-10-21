275b6498f06b287dca57e9827ad84f6b9d4b926a
Owain Lewis <owain@owainlewis.com>
2015-10-21 20:09:31 +0100

    commit sql

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,31 @@
+# Users schema
+
+# --- !Ups
+
+CREATE SEQUENCE incident_id_seq;
+CREATE TABLE Incidents(
+    id bigint NOT NULL DEFAULT nextval('incident_id_seq'),
+    title text NOT NULL,
+    created TIMESTAMP WITH TIME ZONE NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE SEQUENCE update_id_seq;
+CREATE TABLE Updates(
+    id bigint NOT NULL DEFAULT nextval('update_id_seq'),
+    incident bigint NOT NULL,
+    title text NOT NULL,
+    description text,
+    created TIMESTAMP WITH TIME ZONE NOT NULL,
+    FOREIGN KEY (incident) REFERENCES Incidents(id),
+    PRIMARY KEY (id)
+);
+
+
+# --- !Downs
+
+DROP TABLE Incidents;
+DROP SEQUENCE incident_id_seq;
+
+DROP TABLE Updates;
+DROP SEQUENCE incident_id_seq;
