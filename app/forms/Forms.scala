507cb75a9aa6c09a0914e741d40ca0da5c4b9dc2
Owain Lewis <owain@owainlewis.com>
2015-10-24 19:23:21 +0100

    Update Forms.scala

--- app/forms/Forms.scala
+++ app/forms/Forms.scala
@@ -4,14 +4,12 @@ import play.api.data.Form
 import play.api.data.Forms._
 
 object Forms {
-
+  
   val incidentForm = Form(
     mapping("title" -> nonEmptyText, "description" -> optional(text))
-      (IncidentData.apply)(IncidentData.unapply)
-  )
+      (IncidentData.apply)(IncidentData.unapply))
 
   val updateForm = Form(
     mapping("incident" -> longNumber, "title" -> nonEmptyText, "description" -> text)
-      (UpdateData.apply)(UpdateData.unapply)
-  )
+      (UpdateData.apply)(UpdateData.unapply))
 }
