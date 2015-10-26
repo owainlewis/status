ceb070213185674887f7baa37b195fb452c73d02
Owain Lewis <owain@owainlewis.com>
2015-10-26 13:01:56 +0000

    updates config

--- app/config/Config.scala
+++ app/config/Config.scala
@@ -3,16 +3,24 @@ package config
 import play.api.Play
 
 object Config {
+  private def getString(k: String): Option[String] = Play.current.configuration.getString(k)
 
-  def getString(k: String): Option[String] = Play.current.configuration.getString(k)
+  private def getOr(k: String, v: String) = getString(k).getOrElse(v)
+
+  val backgroundEnabled = Play.current.configuration.getBoolean("background.enabled").getOrElse(true)
+
+  /**
+   * The title shown on the main incidents page
+   */
+  val applicationHeader = getOr("app.title", "operations")
 
   /**
    * The admin username
    */
-  val authUser = getString("auth.username").getOrElse("admin")
+  val authUser = getOr("auth.username", "admin")
 
   /**
    * The admin password
    */
-  val authPass = getString("auth.password").getOrElse("password")
+  val authPass = getOr("auth.password", "password")
 }
